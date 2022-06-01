06c66b65ac5f9102f559053588b79cb3af229506
alfilail <nur.alfilail96@gmail.com>
2022-06-01 17:26:07 +0700

    init app module

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/alfilail/app-module
+
+go 1.18
+
+require github.com/alfilail/source-module v1.0.1
+
+require github.com/alfilail/source-module/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	printword "github.com/alfilail/source-module"
+	printword2 "github.com/alfilail/source-module/v2"
+)
+
+func main() {
+	fmt.Println(printword.PrintWord("Uvuvwevwevwe Onyetenyevwe Ugwemubwem Osas"))
+	fmt.Println(printword2.PrintWord("Hai", "Alfi"))
+}
